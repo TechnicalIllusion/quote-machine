ebcdbe61341ba5cd4be0833707eebaab2bf89f61
TechnicalIllusion <robert_coones@yahoo.com>
2020-04-19 17:09:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { QuoteBox } from './QuoteBox';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            quote: {}
+        }
+        this.getQuote = this.getQuote.bind(this);
+    }
+
+    getQuote = async () => {
+        axios.get('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+            .then((res) => {
+                let randomIndex = Math.floor(Math.random() * 102);
+                this.setState({
+                    quote: res.data.quotes[randomIndex]
+                })
+            }
+        )
+    }
+
+    componentDidMount() {
+        this.getQuote();
+    }
+
+    render() {
+        return (
+            <div className="App" id="wrapper">
+                <div id="quote-box">
+                    <QuoteBox quote={this.state.quote.quote} author={this.state.quote.author}/>
+                    <a id="new-quote" onClick={this.getQuote}>New quote</a>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/QuoteBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const QuoteBox = (props) => {
+  let twitterLink = 'https://twitter.com/intent/tweet?text=' + props.quote + ' - ' + props.author;
+  let tumblr = 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(props.author)+'&content=' + encodeURIComponent(props.quote)+'&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
+  return (
+      <div>
+        <p id="text"><i className="fa fa-quote-left"></i>{props.quote}</p>
+        <p id="author">- {props.author}</p>
+        <div id="social-buttons">
+          <a href={twitterLink} className="button"><i className="fa fa-twitter"></i></a>
+          <a href={tumblr} className="button"><i className="fa fa-tumblr"></i></a>
+        </div>
+      </div>
+  )
+}
+
+export default QuoteBox;
