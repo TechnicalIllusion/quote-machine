03fed5661a9b45589912bcb65d4e36b5f61a96a9
TechnicalIllusion <robert_coones@yahoo.com>
2020-04-19 17:54:47 -0500

    (fix): Updated getQuote method to use await

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
     }
 
     getQuote = async () => {
-        axios.get('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+        await axios.get('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
             .then((res) => {
                 let randomIndex = Math.floor(Math.random() * 102);
                 this.setState({
